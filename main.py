30d333f0f221aa968a5abf178ec6b5ae172471d6
Ankita Singh <ankitasingh7639369@gmail.com>
2025-05-18 01:59:58 +0530

    create some route and function

--- main.py
+++ main.py
@@ -1,13 +1,32 @@
 # Import the FastAPI class to create the app instance
 from fastapi import FastAPI 
+from typing import Optional
 # Create a FastAPI application instance
 myapp = FastAPI()
 
 # Define a route for GET requests at the root URL "/
 @myapp.get("/")
 def index():
-    return {'data': {'name': 'Sarthak'}} # Return a JSON response 
+    return {'data': 'blog list'} # Return a JSON response 
 
-@myapp.get("/about")
-def about():
-    return {'data' : {'name': 'Ankita is working in Google'}}
+@myapp.get('/blog')
+def blog(limit = 10, published:bool = True, sort: Optional[str]=None):
+    if published:
+        return {'data': f'{limit} published blogs from the db'}
+    else:
+        return {'data': f'{limit} blogs from the db'}
+
+@myapp.get('/blog/unpublished')
+def unpublished():
+    return {'data' : "all unpublished blogs"}
+
+@myapp.get("/blog/{id}")
+def show(id: int):
+    #fetch blog with id
+    return {'data' : id}
+
+@myapp.get('/blog/{id}/comments')
+def comments(id):
+    #fetch comment of a blog with id
+    return {'data': {'1','2'}}
+
