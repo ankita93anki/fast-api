f8adc639e53ff3ae0e0cd53da47cd6023f29246c
Ankita Singh <ankitasingh7639369@gmail.com>
2025-05-18 01:12:24 +0530

    change app tp myapp

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 # Import the FastAPI class to create the app instance
 from fastapi import FastAPI 
-
 # Create a FastAPI application instance
-app = FastAPI()
+myapp = FastAPI()
 
 # Define a route for GET requests at the root URL "/
-@app.get("/")
+@myapp.get("/")
 def index():
     return {'data': {'name': 'Sarthak'}} # Return a JSON response 
 
-@app.get("/about")
+@myapp.get("/about")
 def about():
     return {'data' : {'name': 'Ankita is working in Google'}}
